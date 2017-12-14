392d27e954c45993fa6c519139c278e9d51e74f9
khlose <schwin60@hotmail.com>
2017-12-14 15:31:42 -0500

    get rid of unnecessary calc

--- detect-track-motion.py
+++ detect-track-motion.py
@@ -3,7 +3,8 @@ import cv2
 from threading import Thread
 import imutils
 
-multithread=False
+#change to disable multithread
+multithread = True
 
 class threadedCamera:
     def __init__(self,src = 0):
@@ -45,10 +46,9 @@ if __name__ == '__main__':
         threadedVid = threadedCamera(src = 0).start()
     else:
         video = cv2.VideoCapture(0)
-
-
     (x, y, w, h) = (0, 0, 0, 0)
     dilated = None
+
     # LOOP
     while True:
         # Check first frame (blocking I/O)
@@ -66,7 +66,6 @@ if __name__ == '__main__':
         # Check for background
         if back is None:
             # Set background to current frame
-
             back = gray
 
         if status == 'motion':
@@ -76,20 +75,14 @@ if __name__ == '__main__':
 
             # Create a threshold to exclude minute movements
             # threshold(src treshold,max value,)
-            thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+            thresh2 = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
 
             # Dialate threshold to further reduce error
-            thresh = cv2.dilate(thresh, dilated, iterations=2)
 
-            thresh2 = cv2.erode(thresh, None, iterations=2)
-            thresh2 = cv2.dilate(thresh2, dilated, iterations=15)
-
-            #cv2.imshow("THRESHOLD", thresh)
-            #cv2.imshow("dilated", thresh2)
+            thresh2 = cv2.erode(thresh2, None, iterations=2)
+            thresh2 = cv2.dilate(thresh2, dilated, iterations=17)
 
             # Check for contours in our threshold
-            #_, cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
             _, cnts, hierarchy2 = cv2.findContours(thresh2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
             # Check each contour
@@ -127,16 +120,6 @@ if __name__ == '__main__':
         if status == 'tracking':
             # Update our tracker
             ok, bbox = tracker.update(frame)
-            # Create a visible rectangle for our viewing pleasure
-            #if ok:
-            if ok:
-                p1 = (int(bbox[0]), int(bbox[1]))
-                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-                cv2.rectangle(frame,p1,p2,(0,0,255),10)
-
-        #Show our webcam
-        #cv2.imshow("Camera",frame)
-
 
         # If we have been tracking for more than a few seconds
         if idle_time >= 10:
@@ -151,7 +134,6 @@ if __name__ == '__main__':
             ok = None
 
             # Recreate tracker
-            # tracker = cv2.TrackerMIL_create()
             tracker = cv2.Tracker_create("KCF")
 
         # Incriment timer
